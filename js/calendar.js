81a014a0f7310e63b0f9ebeed596e5502d0c599b
Mathieu POUSSE <mathieu.pousse@ouest-france.fr>
2019-03-18 23:44:12 +0100

    feat(various): introducing maps and calendar

---
+++ js/calendar.js
@@ -0,0 +1,19 @@
+new HelloWeek({
+    langFolder: 'js/calendar/langs/',
+    daysHighlight: [
+        {
+            days: [
+                ['2019-04-16', '2019-04-24'],
+                ['2019-05-08', '2019-05-14']
+            ],
+            backgroundColor: '#6495ed',
+            color: '#fff',
+            title: 'Summer Holidays'
+        },
+        {
+            days: ['2019-03-24'],
+            backgroundColor: '#f08080',
+            title: 'John Doe Birthday'
+        }
+    ]
+});
