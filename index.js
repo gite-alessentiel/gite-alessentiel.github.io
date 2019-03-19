bb49bef6de21bb5afe166f8c0ef7f58f9407bc3a
Mathieu POUSSE <mathieu.pousse@ouest-france.fr>
2019-03-19 22:40:53 +0100

    chore(parcel): introducing parceljs

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// Initialize and add the map
-export function initMap() {
+import './scss/main.scss';
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+import './js/map';
+// import './js/hello.week.min';
+// import './js/calendar.js';
+
+import('https://maps.googleapis.com/maps/api/js?key=AIzaSyBOKNVB5xJvCxtydI_hipGLoeamhzpp00Y&callback=initMap').then(() => {
+    console.log("prout !");
+});
+
+window.initMap = function() {
     var alessentiel = {lat: 45.2075138, lng: 1.1136933};
     // The map, centered at Grange d'Ans
     var map = new google.maps.Map(
@@ -14,6 +25,7 @@ export function initMap() {
         });
     // The marker, positioned at Grange d'Ans
     var marker = new google.maps.Marker({position: alessentiel, map: map});
+    console.log('yo !');
 }
 
-
+console.log('hello world 2');
