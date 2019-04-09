58f2c2f95041f4853c287ce62f4c09c3bf2c1248
Mathieu POUSSE <mathieu.pousse@ouest-france.fr>
2019-04-10 01:31:10 +0200

    pif

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,18 +1,34 @@
-// import HelloWeek from 'helloweek';
-// import 'hello-week/dist/css/hello.week.min.css';
-// import 'hello-week/dist/css/hello.week.theme.min.css';
-//
-// const reservations = {}; //require('../../reservations.json');
-//
-//
-// new HelloWeek.HelloWeek({
-//     langFolder: 'calendar/langs/',
-//     daysHighlight: [
-//         {
-//             days: reservations,
-//             backgroundColor: '#f08080',
-//             color: '#fff',
-//             title: 'Not available'
-//         }
-//     ]
-// });
+import 'hello-week/dist/hello.week.min';
+import 'hello-week/dist/css/hello.week.min.css';
+import 'hello-week/dist/css/hello.week.theme.min.css';
+
+const reservations = {}; //require('../../reservations.json');
+
+if (document.getElementById("calendar") !== null) {
+
+    new HelloWeek({
+        daysHighlight: [
+            {
+                days: [
+                    "2019-04-08",
+                    "2019-04-09",
+                    "2019-04-10",
+                    "2019-04-11",
+                    "2019-04-12",
+                    "2019-04-13",
+                    "2019-04-14",
+                    "2019-04-15",
+                    "2019-04-16",
+                    "2019-04-17",
+                    "2019-04-18",
+                    "2019-04-19",
+                    "2019-04-20",
+                    "2019-04-21",
+                ],
+                backgroundColor: '#f08080',
+                color: '#fff',
+                title: 'Not available'
+            }
+        ]
+    });
+}
--- src/js/map.js
+++ src/js/map.js
@@ -4,7 +4,7 @@ export default function() {
     // The map, centered at Grange d'Ans
     var map = new google.maps.Map(
         document.getElementById('map'), {
-            zoom: 11,
+            zoom: 10,
             center: alessentiel,
             mapTypeId: 'roadmap',
             mapTypeControlOptions: {
